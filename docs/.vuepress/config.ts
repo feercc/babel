56d3f67e17ad029198a7b5f07f0169ba41478659
任亚楠 <renyanan@sfmail.sf-express.com>
2022-08-23 10:15:37 +0800

    添加gitflow

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,8 @@
+import { defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+  lang: 'zh-CN',
+  title: 'babel中文说明文档 ！',
+  description: '这里是最新的babel中文说明文档，并结合相关事例',
+  base: '/babel/',
+})
